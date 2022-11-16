8fd714956f8651137c2b120d58f178e19fb2068a
pourya ramezani <pourya.ramezani.21224@gmail.com>
2022-11-16 10:28:31 +0330

    "finish part of FloatingActionButton"

--- app/src/main/java/personal/training/material/design/FloatingActionButtonActivity.java
+++ app/src/main/java/personal/training/material/design/FloatingActionButtonActivity.java
@@ -1,27 +1,62 @@
 package personal.training.material.design;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import android.widget.Button;
-import android.widget.Toast;
+import com.google.android.material.snackbar.Snackbar;
 
 public class FloatingActionButtonActivity extends AppCompatActivity {
+    private boolean isFavorite = false;
     private FloatingActionButton floatingActionButtonButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_floating_action_button);
 
         floatingActionButtonButton = findViewById(R.id.fab_main_id);
 
         floatingActionButtonButton.setOnClickListener(v -> {
-//            Toast.makeText("Add")l
-            floatingActionButtonButton.setImageResource(R.drawable.ic_baseline_add_circle_outline_24);
-//            floatingActionButtonButton.get
+            if (!isFavorite) {
+
+                floatingActionButtonButton.setImageResource(R.drawable.ic_baseline_add_circle_outline_24);
+                isFavorite = !isFavorite;
+
+                Snackbar addSnackBarCustomize = Snackbar.make(v, "Add!", Snackbar.LENGTH_LONG);
+
+                View viewAddSnackBar = addSnackBarCustomize.getView();
+                viewAddSnackBar.setBackgroundColor(getResources().getColor(R.color.purple_200, getTheme()));
+
+                addSnackBarCustomize.setTextColor(getColor(R.color.black));
+                addSnackBarCustomize.setActionTextColor(getColor(R.color.black));
+
+                addSnackBarCustomize.setAction("بریم؟", vAdd ->
+                        startActivity(new Intent(this, ThemeActivity.class))
+                ).show();
+            } else {
+
+                floatingActionButtonButton.setImageResource(R.drawable.ic_baseline_add_24);
+                isFavorite = !isFavorite;
+
+                Snackbar removeSnackBarCustomize = Snackbar.make(v, "Remove!", Snackbar.LENGTH_LONG);
+
+                View viewRemoveSnackBar = removeSnackBarCustomize.getView();
+                viewRemoveSnackBar.setBackgroundColor(getResources().getColor(R.color.purple_200, getTheme()));
+
+                removeSnackBarCustomize.setTextColor(getResources().getColor(R.color.black, getTheme()));
+                removeSnackBarCustomize.setActionTextColor(getColor(R.color.black));
+
+                removeSnackBarCustomize.setAction("بریم؟", vRemove ->
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com/")))
+                ).show();
+            }
+
+
         });
     }
 }
