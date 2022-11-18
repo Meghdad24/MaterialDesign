2b63ee31c5c49e9f70ee9cb5e11fc8fffbf407d5
pourya ramezani <pourya.ramezani.21224@gmail.com>
2022-11-18 20:02:17 +0330

    change menu UI

--- app/src/main/java/personal/training/material/design/MenuActivity.java
+++ app/src/main/java/personal/training/material/design/MenuActivity.java
@@ -1,24 +1,25 @@
 package personal.training.material.design;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
-    private Button floatingActionBtn;
-    private Button themeBtn;
-    private Button snackBarBtn;
+    private CardView floatingActionBtn;
+    private CardView themeBtn;
+    private CardView snackBarBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        floatingActionBtn = findViewById(R.id.btn_fab_menu_id);
-        themeBtn = findViewById(R.id.btn_theme_menu_id);
-        snackBarBtn = findViewById(R.id.btn_snkb_menu_id);
+        floatingActionBtn = findViewById(R.id.cv_fab_menu_id);
+        themeBtn = findViewById(R.id.cv_bg_menu_id);
+        snackBarBtn = findViewById(R.id.cv_1_menu_id);
 
         floatingActionBtn.setOnClickListener(v -> {
             Intent intent = new Intent(this, FloatingActionButtonActivity.class);
